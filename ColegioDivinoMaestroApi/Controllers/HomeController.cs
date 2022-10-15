6a7e5833b489fecb82b5d5f7c2df9bcd5a27bcf1
LAB_H_REAL <labh@continental.edu.pe>
2022-10-14 21:30:52 -0500

    nuevo controlador home

---
+++ ColegioDivinoMaestroApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColegioDivinoMaestroApi.Controllers
+{
+    [Controller]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult<object> HomePage()
+        {
+            return Ok(new { hola = "mundo"});
+        }
+    }
+}
